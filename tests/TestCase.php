c4d51bb1ac09a89ae2d50794a3b85360671816c8
iyngaran <iyngaran@users.noreply.github.com>
2021-03-20 16:05:38 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Iyngaran\Location\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Iyngaran\Location\LocationServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
